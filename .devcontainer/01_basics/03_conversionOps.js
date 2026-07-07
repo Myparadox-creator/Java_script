efbc80890161635f07508539b4958a039cc9427b
Aditya R. Satapathy <adityaranjanwxd@gmail.com>
2026-07-07 07:07:21 +0000

    conversion operations

---
+++ .devcontainer/01_basics/03_conversionOps.js
@@ -0,0 +1,27 @@
+// Investigating conversions
+let score = null
+
+console.log(typeof (score))
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// conversion of number to boolean
+
+let isloggedIn = 1
+let booleanisloggedIn = Boolean(isloggedIn)
+
+console.log(booleanisloggedIn)
+console.log(typeof booleanisloggedIn)
+
+//conversion of number to string
+
+let somenum = 55
+
+let stringnum = String(somenum)
+console.log(stringnum)
+console.log(typeof stringnum)
