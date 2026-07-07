2ee6c2b4986643d8a1aa5477a4ea4acd7605b8e7
Aditya R. Satapathy <adityaranjanwxd@gmail.com>
2026-07-07 06:23:03 +0000

    commit changes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId= 1234444
+let accountEmail= "aditya2005dc@gmail.com"
+var accountPassword= "1234"
+accountCity = "Rourkela"
+
+let AccountState;
+
+//accountId= 34 ---> not allowed because accountId is a constant variable
+accountEmail = "aditya@gmail.com"
+accountPassword = "123"
+accountCity = "Bbsr"
+
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, AccountState])
