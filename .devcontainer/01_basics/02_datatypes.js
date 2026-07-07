09ae0210b5a80c2aece6adff7f2f3aa2624033af
Aditya R. Satapathy <adityaranjanwxd@gmail.com>
2026-07-07 06:41:31 +0000

    initial commit

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // helps to use modern JavaScript features and avoid common mistakes
+//alert(3+3)  // we are using alert to show the result of 3+3 in a popup window but it's node js show the popup window is not supported in node js so we will use console.log instead
+console.log(3 + 3); 
+console.log("hello world"); 
+
+
+let name = "Aditya"
+let age = 21
+let isloggedin = true
+let state;
+let stste = null
+
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isloggedin)
+console.log(typeof state)
+console.log(typeof stste)
