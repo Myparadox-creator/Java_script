b5afef652d4cd70b31924dde7eafa58b892acb58
Aditya R. Satapathy <adityaranjanwxd@gmail.com>
2026-07-08 14:09:31 +0000

    datatype summary

---
+++ .devcontainer/01_basics/datatypes_summary.js
@@ -0,0 +1,33 @@
+const score = 100
+const scorevalue = 100.3
+
+const isloggedin = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('1234')
+const anotherId = Symbol('1234')
+console.log(id === anotherId);
+
+// Reference (non primitive)
+
+// Array, objectd, functions
+const bigNumber = 12333332728n
+
+
+const heros = ['thor', 'spiderman', 'ironman']
+
+let myobj ={
+    name: "thor",
+    age:  1000
+}
+
+
+// Functions
+
+const myfunction = function(){
+    console.log("hello world")
+}
+
+console.log(typeof myfunction)
+console.log(typeof bigNumber)
