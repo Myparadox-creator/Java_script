7dfd3e7bcc9d4d09eac63238817463772a699e48
Aditya R. Satapathy <adityaranjanwxd@gmail.com>
2026-08-24 20:17:52 +0530

    math and num

---
+++ .devcontainer/01_basics/05_nums_maths.js
@@ -0,0 +1,39 @@
+const score = 400
+console.log(score);
+
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2));
+
+const otherNum = 123.789
+console.log(otherNum.toPrecision(4));
+
+
+const hundred = 1000000
+console.log(hundred.toLocaleString('en-IN'));
+
+
+//+++++++++++++++++ Maths +++++++++++++++++
+
+
+console.log(Math);
+console.log(Math.abs(-6));
+console.log(Math.round(6.8));
+console.log(Math.ceil(3.1));
+console.log(Math.floor(4.8));
+console.log(Math.min(2, 3, 4, 5, 6, 7, 8, 9));
+console.log(Math.max(2, 3, 4, 5, 6, 7, 8, 9));
+console.log((Math.random()* 10) + 1)
+
+
+//trick
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
