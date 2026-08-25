61bc9876e3d211eb67c81ac350714a42488ea5a1
Aditya R. Satapathy <adityaranjanwxd@gmail.com>
2026-08-25 17:44:52 +0530

    arrays in js

---
+++ .devcontainer/02_basics/01_arrays.js
@@ -0,0 +1,39 @@
+// arrays
+
+const arr = [0, 1, 2, 3, 4, 5]
+const myheros = ["ironman", "Thor", "doom", "void"]
+
+console.log(arr[0])
+
+const myArr = new Array(1,2,3,4)
+
+
+// array methods
+
+//arr.push(6)
+//arr.push(7)
+//arr.pop()
+
+arr.unshift(9)
+arr.shift()
+
+console.log(arr.includes(9))
+console.log(arr.indexOf(3))
+
+const newAr = arr.join()
+
+console.log(arr);
+console.log(newAr)
+
+
+// slice, spice
+
+console.log("A ", arr);
+
+const myn1 = arr.slice(1,3)
+
+console.log(myn1)
+console.log("B ", arr)
+
+const myn2 = arr.splice(1,3)
+console.log(myn2);
