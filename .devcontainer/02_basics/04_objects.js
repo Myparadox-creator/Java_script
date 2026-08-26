a03f3af3b102c045241b084a62e5b499f85edd90
Aditya R. Satapathy <adityaranjanwxd@gmail.com>
2026-08-26 20:35:17 +0530

    objects in js 4

---
+++ .devcontainer/02_basics/04_objects.js
@@ -0,0 +1,63 @@
+const googleuser = new Object()  // singleton object
+const googleuser2 = {}  // non-singleton object
+
+googleuser.id = "1234dc"
+googleuser.name = "Aditya"
+googleuser.isLoggIn = false
+
+//console.log(googleuser)
+
+const regularuser = {
+    email: "asis@example.com",
+    fullname: {
+        userfullname: {
+        firstname: "Aditya",
+        lastname: "Satapathy"
+        }
+    }
+}
+
+
+//console.log(regularuser.fullname.userfullname.lastname)
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj4 = {5: "e", 6: "f"}
+
+//const obj3 = {obj1, obj2}
+//const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+
+//console.log(obj3);
+
+const users = [
+    {
+    id: 1,
+    email: "aditya@ex.com"
+},
+{
+    id: 1,
+    email: "aditya@ex.com"
+},
+{
+    id: 1,
+    email: "aditya@ex.com"
+},{
+    id: 1,
+    email: "aditya@ex.com"
+}
+]
+
+users[1].email
+
+console.log(googleuser)
+
+console.log(Object.keys(googleuser))
+console.log(Object.values(googleuser))
+console.log(Object.entries(googleuser))
+
+console.log(googleuser.hasOwnProperty('isLoggIn'));
+
+
+
