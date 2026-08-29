8d2e2f47f1431b962152431876a3ed4cc3eff6b2
Aditya R. Satapathy <adityaranjanwxd@gmail.com>
2026-08-29 19:39:46 +0530

    forof loop

---
+++ .devcontainer/05_iterations/three.js
@@ -0,0 +1,38 @@
+// for of loop
+
+const arr = [1,2,3,4]
+
+for (const i of arr) {
+    //console.log(i);
+    
+}
+
+const greetings = "hello world!"
+for(const greet of greetings){
+    console.log(`Each character is ${greet}`);
+    
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United states")
+map.set('FR', "France")
+
+console.log(map)
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+    
+}
+
+const myObject = {
+    'game1': 'NFS',
+    'game2': 'Hitman'
+}
+
+for (const [key, value] of myObject) {
+    console.log(key, ':-', value);  // objects aren't iteratable
+    
+}
